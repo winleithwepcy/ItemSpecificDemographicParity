76ac92c80bc1ea8ae59381c1d367e62741aa8fe1
winleithwepcy <31789643+winleithwepcy@users.noreply.github.com>
2026-01-09 22:11:34 +0900

    Delete Downloads/ItemSpecificDemographicParity directory

--- Downloads/ItemSpecificDemographicParity/plot/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv('log/tuning_results_FairPMF_seed2053.csv')
-plt.plot(df['fair_lambda'], df['best_hr'], label='HR@10')
-plt.plot(df['fair_lambda'], df['avg_value'], label='Value Unfairness')
-plt.xscale('log')
-plt.xlabel('fair_lambda')
-plt.legend()
-plt.show()
